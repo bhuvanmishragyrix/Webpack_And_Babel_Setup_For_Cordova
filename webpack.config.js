0f1046411d236515938e720a85970f641f39e458
Bhuvan Mishra <bhuvan.mishra@gyrix.in>
2018-11-14 11:58:27 +0530

    Removed some unnecessary code from index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'www'),
-    filename: 'bundle.js'
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'www'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 };
